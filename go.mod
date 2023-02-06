c55aa5c4f212bcdf3b1451fed132c5f3bcc32d96
David Rivera Morales <driveramorales46@gmail.com>
2023-02-06 05:59:29 -0600

    flags handling implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module termies.mxdui.com
 
 go 1.20
+
+require github.com/mattn/go-sqlite3 v1.14.16 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,55 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// make an example cli app
-
 func main() {
-	fmt.Println("Hello World!")
+
+	// on the first run, the default menu is displayed
+
+	fmt.Println("Welcome to the program")
+
+	// make the file to store the data in the current directory
+
+	// create file if it doesn't exist
+	if _, err := os.Stat("./data.db"); os.IsNotExist(err) {
+		file, err := os.Create("./data.db")
+		checkErr(err)
+		defer file.Close()
+	}
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	checkErr(err)
+
+	fmt.Println(db)
+
+	var menu = flag.String("menu", "default", "menu name")
+
+	flag.Parse()
+
+	switch *menu {
+	case "default":
+		// do something
+	case "debug":
+		// do something
+
+	case "report":
+		// do something
+	case "help":
+		// do something
+
+	}
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
